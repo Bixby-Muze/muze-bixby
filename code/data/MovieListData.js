9267565fea173bfb919ca381eefc9383285f4331
yoogle96 <dmltjs851@gmail.com>
2020-01-18 16:00:38 +0900

    Add movie list sample data

---
+++ code/data/MovieListData.js
@@ -0,0 +1,214 @@
+// 상점 가게 정보 데이터
+module.exports = [
+  {
+      "movieCd": "20197691",
+      "movieNm": "잃어버린 세계를 찾아서",
+      "movieNmEn": "Missing Link",
+      "prdtYear": "2019",
+      "openDt": "20191009",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "미국",
+      "genreAlt": "애니메이션,어드벤처",
+      "repNationNm": "미국",
+      "repGenreNm": "애니메이션",
+      "directors": [
+          {
+              "peopleNm": "크리스 버틀러"
+          }
+      ],
+      "companys": []
+  },
+  {
+      "movieCd": "20100130",
+      "movieNm": "공기인형",
+      "movieNmEn": "Air Doll",
+      "prdtYear": "2009",
+      "openDt": "20100408",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "일본",
+      "genreAlt": "드라마,판타지",
+      "repNationNm": "일본",
+      "repGenreNm": "드라마",
+      "directors": [
+          {
+              "peopleNm": "고레에다 히로카즈"
+          }
+      ],
+      "companys": [
+          {
+              "companyCd": "20101897",
+              "companyNm": "아스믹에이스엔터테인먼트"
+          }
+      ]
+  },
+  {
+      "movieCd": "20204244",
+      "movieNm": "파라다이스 인 푸켓:유학생의 맛 무삭제판",
+      "movieNmEn": "",
+      "prdtYear": "2019",
+      "openDt": "20200116",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "한국",
+      "genreAlt": "멜로/로맨스,드라마",
+      "repNationNm": "한국",
+      "repGenreNm": "멜로/로맨스",
+      "directors": [
+          {
+              "peopleNm": "윤준세"
+          }
+      ],
+      "companys": [
+          {
+              "companyCd": "20114899",
+              "companyNm": "(주)영진크리에이티브"
+          }
+      ]
+  },
+  {
+      "movieCd": "20204243",
+      "movieNm": "파라다이스 인 푸켓:여대생의 맛 무삭제판",
+      "movieNmEn": "",
+      "prdtYear": "2019",
+      "openDt": "20200116",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "한국",
+      "genreAlt": "멜로/로맨스,드라마",
+      "repNationNm": "한국",
+      "repGenreNm": "멜로/로맨스",
+      "directors": [
+          {
+              "peopleNm": "윤준세"
+          }
+      ],
+      "companys": [
+          {
+              "companyCd": "20114899",
+              "companyNm": "(주)영진크리에이티브"
+          }
+      ]
+  },
+  {
+      "movieCd": "20204227",
+      "movieNm": "친구의 육덕진 딸 첫경험 시키기 ",
+      "movieNmEn": "Daughter of subordinate",
+      "prdtYear": "2019",
+      "openDt": "20200116",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "일본",
+      "genreAlt": "드라마,성인물(에로)",
+      "repNationNm": "일본",
+      "repGenreNm": "드라마",
+      "directors": [],
+      "companys": []
+  },
+  {
+      "movieCd": "20191799",
+      "movieNm": "주말 오후6시의 연인",
+      "movieNmEn": "",
+      "prdtYear": "2019",
+      "openDt": "20200116",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "한국",
+      "genreAlt": "멜로/로맨스",
+      "repNationNm": "한국",
+      "repGenreNm": "멜로/로맨스",
+      "directors": [
+          {
+              "peopleNm": "왕지방"
+          }
+      ],
+      "companys": [
+          {
+              "companyCd": "20123227",
+              "companyNm": "(주)나우콘텐츠"
+          }
+      ]
+  },
+  {
+      "movieCd": "20193647",
+      "movieNm": "우아한 만찬",
+      "movieNmEn": "Beatriz at Dinner",
+      "prdtYear": "2017",
+      "openDt": "20200116",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "미국",
+      "genreAlt": "코미디,드라마",
+      "repNationNm": "미국",
+      "repGenreNm": "코미디",
+      "directors": [
+          {
+              "peopleNm": "미구엘 아테타"
+          }
+      ],
+      "companys": []
+  },
+  {
+      "movieCd": "20193862",
+      "movieNm": "엄마 바람피게하기 ",
+      "movieNmEn": "",
+      "prdtYear": "2019",
+      "openDt": "20200116",
+      "typeNm": "장편",
+      "prdtStatNm": "개봉",
+      "nationAlt": "한국",
+      "genreAlt": "코미디,드라마",
+      "repNationNm": "한국",
+      "repGenreNm": "코미디",
+      "directors": [
+          {
+              "peopleNm": "윤원규"
+          }
+      ],
+      "companys": [
+          {
+              "companyCd": "20101135",
+              "companyNm": "주식회사 마운틴픽쳐스"
+          }
+      ]
+  },
+  {
+      "movieCd": "20204116",
+      "movieNm": "데빌 인 더 캐빈",
+      "movieNmEn": "Cabin 28",
+      "prdtYear": "2017",
+      "openDt": "",
+      "typeNm": "장편",
+      "prdtStatNm": "기타",
+      "nationAlt": "미국",
+      "genreAlt": "공포(호러)",
+      "repNationNm": "미국",
+      "repGenreNm": "공포(호러)",
+      "directors": [
+          {
+              "peopleNm": "앤드류 존스"
+          }
+      ],
+      "companys": []
+  },
+  {
+      "movieCd": "20204201",
+      "movieNm": "다운시커",
+      "movieNmEn": "Dawnseeker",
+      "prdtYear": "2018",
+      "openDt": "",
+      "typeNm": "장편",
+      "prdtStatNm": "기타",
+      "nationAlt": "미국",
+      "genreAlt": "SF",
+      "repNationNm": "미국",
+      "repGenreNm": "SF",
+      "directors": [
+          {
+              "peopleNm": "저스틴 프라이스"
+          }
+      ],
+      "companys": []
+  }
+]
