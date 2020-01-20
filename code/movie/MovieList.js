632ea4c52c5673e0587de6f464d987cff4897944
yoogle96 <dmltjs851@gmail.com>
2020-01-20 17:26:53 +0900

    Update MovieList

--- code/movie/MovieList.js
+++ code/movie/MovieList.js
@@ -1,9 +1,23 @@
+var http = require('http');
+var config = require('config');
+var console = require('console');
+
 module.exports.function = function movieList () {
-  // Movie sample data
-  const fakeData = require("../data/MovieListData");
-  const console = require("console");
+  
+  var options = {
+    format: 'json',
+    query: {
+      movieNm: "",
+      directorNm: "",
+      openStartDt: "2019",
+      openEndDt: "2019"
+    }
+  };
 
-  console.log(fakeData);
+  const fakeData = http.getUrl(config.get("apiBaseUrl")+"movieList", options);
+  console.log(fakeData['movieListResult']['movieList'])
+  // console.log(config.get('baseUrl'))
+  // console.log(fakeData);
 
-  return fakeData
+  return fakeData['movieListResult']['movieList']
 }
