463b1598ddaf3e712d1962fe28da24d2d62b4791
sudosubin <sudosubin+github@gmail.com>
2020-01-21 17:21:50 +0900

    add list view search feature

--- code/movie/MovieList.js
+++ code/movie/MovieList.js
@@ -1,24 +1,41 @@
 var http = require('http');
 var config = require('config');
 var console = require('console');
+var tool = require('lib/tool.js');
 
-module.exports.function = function movieList (_movieNm, _directorNm, _openStartDt, _openEndDt) {
-  
-  // var options = {
-  //   format: 'json',
-  //   query: {
-  //     movieNm: "",
-  //     directorNm: _directorNm,
-  //     openStartDt: "",
-  //     openEndDt: ""
-  //   }
-  // };
+module.exports.function = function dailyBoxOffice () {
 
-  // const fakeData = http.getUrl(config.get("apiBaseUrl")+"movieList", options);
-  // console.log(fakeData['movieListResult']['movieList'])
-  // // console.log(config.get('baseUrl'))
-  // // console.log(fakeData);
+  var options = {
+    format: 'json',
+    headers: {
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJvb2V1bnoiLCJVU0VSTkFNRSI6Im9vZXVueiIsIkVNQUlMIjoieXVuczk5NEBnbWFpbC5jb20iLCJVU0VSX1JPTEUiOiJST0xFX1VTRVIifQ.Ibvyggk8HMcgY-hiChQNb5TzOGcKH8KKAJAgx-Fto7s'
+    },
+    query: {
+      openStartDt: '',
+      openEndDt: '',
+      directorNm: '',
+      movieNm: '어벤져스',
+    }
+  };
 
-  // return fakeData['movieListResult']['movieList']
-  return require("../data/MovieListData.js");
+  var resData = http.getUrl(config.get("apiBaseUrl")+"movieList", options);
+
+  let resultData = resData.data.movieListResult.movieList;
+
+  for(let i = 0; i < resultData.length; i++){
+    let movieCd = resultData[i].movieCd
+    let movieDetail = tool.GetMovieDetail(movieCd);
+
+    resultData[i].showTm = movieDetail.movieInfoResult.movieInfo.showTm;
+    resultData[i].nations = movieDetail.movieInfoResult.movieInfo.nations;
+    resultData[i].genres = movieDetail.movieInfoResult.movieInfo.genres;
+    resultData[i].directors = movieDetail.movieInfoResult.movieInfo.directors;
+    resultData[i].actors = movieDetail.movieInfoResult.movieInfo.actors;
+    resultData[i].showTypes = movieDetail.movieInfoResult.movieInfo.showTypes;
+    resultData[i].audits = movieDetail.movieInfoResult.movieInfo.audits;
+  }
+
+  console.log(resultData)
+
+  return resultData;
 }
